bdf38ed8c2442a4f0ad07518fc4ed87a66593952
Mikhail Krotov <mikhail_affog@icloud.com>
2019-05-24 16:43:04 +0300

    added test mongo connection

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, Response, Request
+from flask import Flask, Response, request
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
 import shell_scripts
+
 
 app = Flask(__name__)
 main_path = '/api/v1'
@@ -15,6 +18,20 @@ def get_rabbit():
 @app.route(main_path+'/get_mongo', methods=['GET'])
 def get_mongo():
     return Response(shell_scripts.MONGO_SCRIPT, mimetype='text/plan')
+
+@app.route(main_path + '/test_mongo', methods=['GET'])
+def test_mongo():
+    host, port = request.args.get('host', default='localhost'), request.args.get('port', default='27017')
+    uri = "mongodb://%s:%s/admin" % (host, port)
+    client = MongoClient(uri, maxPoolSize=1, connectTimeoutMS=3000, serverSelectionTimeoutMS=3000)
+    resp = Response('false', mimetype='text/plain')
+    try:
+        if client.admin.command('ismaster')['ok'] == 1:
+            resp = Response('true', mimetype='text/plain')
+    except ConnectionFailure:
+        resp = Response('false', mimetype='text/plain')
+
+    return resp
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
